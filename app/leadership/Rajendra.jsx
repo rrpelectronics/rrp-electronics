b636b71541947cd74809d050614af6563b7988d5
parakh-stuvio001 <parakh@stuvio.co>
2025-08-18 10:02:15 +0530

    Ou Journey & Leadership Completed

---
+++ app/leadership/Rajendra.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rajendra = () => {
+  return (
+    <section className='h-fit w-full'>
+
+    </section>
+  )
+}
+
+export default Rajendra
---
+++ app/leadership/page.js
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react';
+import Banner from '@/app/components/Banner';
+import Text from '@/app/components/Text';
+import Leaders from './Leaders';
+
+const page = () => {
+  return (
+    <main className='"min-h-screen w-full relative"'>
+      <Banner
+        imgSrc={"/images/leadership/banner.webp"}
+        heading={"Leaders of Innovation, \n Architects of the Future"}
+      />
+      <Text
+        text={
+          "Our leadership team brings decades of expertise in semiconductor R&D, advanced manufacturing, and global supply chain management, shaping the future of high-performance electronics."
+        }
+      />
+      <Leaders/>
+    </main>
+  );
+}
+
+export default page
