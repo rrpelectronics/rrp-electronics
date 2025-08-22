ba5673590be584bfb952b8d6bec34c6d47f83e66
parakh-stuvio001 <parakh@stuvio.co>
2025-08-22 14:15:04 +0530

    Added Header Dropdown

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,15 +1,41 @@
 "use client";
 import React, { forwardRef } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import UseDropdownHandlers from "@/app/hooks/UseDropdownHandlers";
+import UseMobileDetection from "../hooks/UseMobileDetection";
+import Dropdown from "./header/Dropdown";
+
+const companyLinks = [
+  { href: "/about", label: "About Us" },
+  { href: "/our-journey", label: "Our Journey" },
+  { href: "/leadership", label: "Leadership" },
+];
+
+const solutionsLinks = [
+  { href: "/solutions/#legacy-packaging", label: "Legacy Packaging" },
+  { href: "/solutions/#advance-packaging", label: "Advanced Packaging" },
+  { href: "/solutions/#display-technologies", label: "Display Technologies" },
+];
+
+const operationsLinks = [
+  { href: "/compliances", label: "Quality Standards & Compliances" },
+  { href: "/logistics", label: "Supply Chain & Logistics" },
+  { href: "/traceability", label: "Traceability" },
+];
 
 const Header = forwardRef((props, ref) => {
+  const pathname = usePathname();
+  const isMobile = UseMobileDetection();
+  const dropdowns = UseDropdownHandlers(isMobile);
+
   return (
-    <header ref={ref} className="will-change-transform fixed top-0 left-0 w-full h-fit py-4 md:py-5 px-3.5 md:px-5 lg:px-10 bg-[#17171780] z-50 backdrop-blur-[2.5rem]">
+    <header
+      ref={ref}
+      className="will-change-transform fixed top-0 left-0 w-full h-fit py-4 md:py-5 px-3.5 md:px-5 lg:px-10 bg-[#17171780] z-50 backdrop-blur-[4px]"
+    >
       <nav className="flex justify-between items-center">
-        <Link
-          href={"/"}
-          className="aspect-[240/26] w-21.5 h-7 lg:w-30 lg:h-9"
-        >
+        <Link href={"/"} className="aspect-[240/26] w-21.5 h-7 lg:w-30 lg:h-9">
           <img
             src="/images/common/rrplogo.png"
             alt="RRP Electronics"
@@ -17,106 +43,53 @@ const Header = forwardRef((props, ref) => {
           />
         </Link>
         <ul className="hidden lg:flex justify-center items-center lg:gap-2 xl:gap-6">
-          <li className="cursor-pointer relative p-2 text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize flex justify-center items-center gap-1">
-            Company
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <mask
-                id="mask0_401_543"
-                maskUnits="userSpaceOnUse"
-                x="0"
-                y="0"
-                width="24"
-                height="24"
-              >
-                <rect width="24" height="24" fill="#646464" />
-              </mask>
-              <g mask="url(#mask0_401_543)">
-                <path
-                  d="M11.9992 14.7069L6.69141 9.39916L7.39916 8.69141L11.9992 13.2914L16.5992 8.69141L17.3069 9.39916L11.9992 14.7069Z"
-                  fill="white"
-                  fillOpacity="0.6"
-                />
-              </g>
-            </svg>
-          </li>
-          <li className="cursor-pointer relative p-2 text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize flex justify-center items-center gap-1">
-            Solutions
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <mask
-                id="mask0_401_543"
-                maskUnits="userSpaceOnUse"
-                x="0"
-                y="0"
-                width="24"
-                height="24"
-              >
-                <rect width="24" height="24" fill="#646464" />
-              </mask>
-              <g mask="url(#mask0_401_543)">
-                <path
-                  d="M11.9992 14.7069L6.69141 9.39916L7.39916 8.69141L11.9992 13.2914L16.5992 8.69141L17.3069 9.39916L11.9992 14.7069Z"
-                  fill="white"
-                  fillOpacity="0.6"
-                />
-              </g>
-            </svg>
-          </li>
-          <li className="cursor-pointer relative p-2 text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize flex justify-center items-center gap-1">
-            Operations
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <mask
-                id="mask0_401_543"
-                maskUnits="userSpaceOnUse"
-                x="0"
-                y="0"
-                width="24"
-                height="24"
-              >
-                <rect width="24" height="24" fill="#646464" />
-              </mask>
-              <g mask="url(#mask0_401_543)">
-                <path
-                  d="M11.9992 14.7069L6.69141 9.39916L7.39916 8.69141L11.9992 13.2914L16.5992 8.69141L17.3069 9.39916L11.9992 14.7069Z"
-                  fill="white"
-                  fillOpacity="0.6"
-                />
-              </g>
-            </svg>
-          </li>
-          <li className="text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
+
+          <Dropdown
+            isOpen={dropdowns.isCompanyOpen}
+            onMouseEnter={() => dropdowns.handleMouseEnter("company")}
+            onMouseLeave={() => dropdowns.handleMouseLeave("company")}
+            label="Company"
+            links={companyLinks}
+            pathname={pathname}
+            onDropdownLinkClick={() => dropdowns.setIsCompanyOpen(false)}
+          />
+
+          <Dropdown
+            isOpen={dropdowns.isSolutionsOpen}
+            onMouseEnter={() => dropdowns.handleMouseEnter("solutions")}
+            onMouseLeave={() => dropdowns.handleMouseLeave("solutions")}
+            label="Solutions"
+            links={solutionsLinks}
+            pathname={pathname}
+            onDropdownLinkClick={() => dropdowns.setIsSolutionsOpen(false)}
+          />
+
+          <Dropdown
+            isOpen={dropdowns.isOperationsOpen}
+            onMouseEnter={() => dropdowns.handleMouseEnter("operations")}
+            onMouseLeave={() => dropdowns.handleMouseLeave("operations")}
+            label="Operations"
+            links={operationsLinks}
+            pathname={pathname}
+            onDropdownLinkClick={() => dropdowns.setIsOperationsOpen(false)}
+          />
+
+          <li className="cursor-pointer text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
             <Link href={"#"} className="p-2">
               Projects
             </Link>
           </li>
-          <li className="text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
+          <li className="cursor-pointer text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
             <Link href={"#"} className="p-2">
               News & Events
             </Link>
           </li>
-          <li className="text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
+          <li className="cursor-pointer text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
             <Link href={"#"} className="p-2">
               Careers
             </Link>
           </li>
-          <li className="text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
+          <li className="cursor-pointer text-[16px] text-white transition-colors ease-in-out hover:text-primary font-neueMontreal leading-[120%] capitalize">
             <Link href={"#"} className="p-2 pr-0 ">
               Contact
             </Link>
