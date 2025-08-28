39f4d64ddcd70fb56fd51ae0ec867d9766cd091b
parakh-stuvio001 <parakh@stuvio.co>
2025-08-28 18:08:24 +0530

    removed unwanted image next-component

--- app/components/Popup.jsx
+++ app/components/Popup.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
 import UseBodyScrollLock from "@/app/hooks/UseBodyScrollLock";
 
 export default function Popup() {
