fef8edd5d82fa96d9826085f6d1cec596a7b092b
parakh-stuvio001 <parakh@stuvio.co>
2025-09-26 14:52:02 +0530

    updated config instead of rewrite we redirected for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: "/admin/:path*",
-        destination: "https://rrp-electronics-cms.vercel.app//:path*", // backend URL
+        destination: "https://rrp-electronics-cms.vercel.app/:path*", // backend URL
+        permanent: false,
       },
     ];
   },
